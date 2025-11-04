913d72f9471474eb9d2ddb5fd4aeb6bbbdf014b0
lorasvlr <lora.lorinet@gmail.com>
2025-11-04 17:45:52 +0100

    test

--- views/backoffice/CreerCompteVendeur.php
+++ views/backoffice/CreerCompteVendeur.php
@@ -5,10 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">  
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-    <link rel="stylesheet" href="../../public/style/styleTemplate.css">
+    <link rel="stylesheet" href="../../public/style.css">
     <title>Création d'un compte vendeur</title>
 </head>
-<body>
-    
+<body class="CreerCompteVendeur">
+    <img src="../../public/images/pdp_user.svg" alt="photo de profil">
+    <h1>Création de votre compte vendeur</h1>
 </body>
 </html>
