b3a84898f259ac32c6f5d8ef69fd10ef18c1d3c8
marechalg <guewenmarechal@protonmail.com>
2025-11-05 19:51:39 +0100

    started aside

---
+++ views/backoffice/partials/aside.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+
+        <link rel="stylesheet" href="/public/style.css">
+    </head>
+
+    <body>
+        <?php require_once './headerMain.php' ?>
+
+        <aside class="backoffice">
+
+        </aside>
+
+        <main class="aside">
+            <h1>enorme CACA boudin</h1>
+        </main>
+
+        <?php require_once './footer.php' ?>
+    </body>
+</html>
