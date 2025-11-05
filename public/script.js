a87bba223f2b75d5808b5912bb7d50ef2b10f5eb
marechalg <guewenmarechal@protonmail.com>
2025-11-05 23:10:47 +0100

    inverted condition to fix the issue

--- public/script.js
+++ public/script.js
@@ -2,7 +2,7 @@
 Array.from(document.getElementsByClassName('aside-btn')).forEach(asideButton => {
     asideButton.addEventListener('click', () => {
         const category = asideButton.children[0].children[1].innerHTML.toLowerCase();
-        if (asideButton.className.includes('here')) {
+        if (!asideButton.className.includes('here')) {
             window.location.href = `./${category}.php`;
         }
     });
--- views/scripts/backoffice/aside.ts
+++ views/scripts/backoffice/aside.ts
@@ -1,7 +1,7 @@
 Array.from(document.getElementsByClassName('aside-btn')).forEach(asideButton => {
     asideButton.addEventListener('click', () => {
         const category: string = asideButton.children[0].children[1].innerHTML.toLowerCase();
-        if (asideButton.className.includes('here')) {
+        if (!asideButton.className.includes('here')) {
             window.location.href = `./${category}.php`;
         }
     })
