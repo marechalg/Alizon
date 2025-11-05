95a01dc4259a4e31cf86ddf51af82f37f4aad53c
marechalg <guewenmarechal@protonmail.com>
2025-11-05 22:40:27 +0100

    Aside backoffice

--- views/backoffice/partials/aside.php
+++ views/backoffice/partials/aside.php
@@ -8,17 +8,58 @@
         <link rel="stylesheet" href="/public/style.css">
     </head>
 
-    <body>
+    <body class="backoffice">
         <?php require_once './headerMain.php' ?>
 
         <aside class="backoffice">
-
+            <ul>
+                <li class="aside-btn here">
+                    <figure>
+                        <img src="/public/images/homeDarkBlue.svg">
+                        <figcaption>Accueil</figcaption>
+                    </figure>
+                </li>
+                <li class="aside-btn">
+                    <figure>
+                        <img src="/public/images/courbeDark.svg">
+                        <figcaption>Statistiques</figcaption>
+                    </figure>
+                </li>
+                <li class="aside-btn">
+                    <figure>
+                        <img src="/public/images/cartDarkBlue.svg">
+                        <figcaption>Produits</figcaption>
+                    </figure>
+                </li>
+                <li class="aside-btn">
+                    <figure>
+                        <img src="/public/images/boiteDark.svg">
+                        <figcaption>Stock</figcaption>
+                    </figure>
+                </li>
+                <li class="aside-btn">
+                    <figure>
+                        <img src="/public/images/chatDark.svg">
+                        <figcaption>Avis</figcaption>
+                    </figure>
+                </li>
+                <li class="aside-btn">
+                    <figure>
+                        <img src="/public/images/cartCheckDark.svg">
+                        <figcaption>Commandes</figcaption>
+                    </figure>
+                </li>
+                <li class="aside-btn">
+                    <figure>
+                        <img src="/public/images/enveloppeDark.svg">
+                        <figcaption>Retours</figcaption>
+                    </figure>
+                </li>
+            </ul>
         </aside>
 
-        <main class="aside">
-            <h1>enorme CACA boudin</h1>
-        </main>
-
         <?php require_once './footer.php' ?>
+
+        <script type="module" src="/public/script.js"></script>
     </body>
 </html>
