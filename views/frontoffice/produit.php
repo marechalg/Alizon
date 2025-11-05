454c49b8a39917757313a30935b68865c1bf4254
Zelirety <natuslanus@gmail.com>
2025-11-05 13:22:03 +0200

    ça marche et je suis heureux :)

---
+++ views/frontoffice/produit.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Page du produit</title>
+    <link rel="stylesheet" href="../../public/style.css">
+</head>
+<body class="pageProduit">
+<header>
+<div class ="rect">
+</div>
+</header>
+<main>
+<section class="infoHautProduit">
+    <article class="rectangleProduit">
+        <img src="flecheGauche.svg" alt="">
+        <div>
+            <img id="imageBouteille" src="Image_bouteille.svg" alt="">
+            <div>
+                <div class="cercleNoir"></div>
+                <div class="cercleNoir"></div>
+                <div class="cercleNoir"></div>
+            </div>
+        </div>
+        <img src="flecheDroite.svg" alt="">
+    </article>
+    <article class="infoPreviewProduit">
+        <h1>Cidre coco d'issé</h1>
+        <div>
+            <h2>29.99€</h2>
+            <h3>40.99€</h3>
+        </div>
+        <h2>Description de l'article :</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus enim iure ratione voluptates
+            eius doloremque obcaecati dignissimos ea porro exercitationem ex omnis reiciendis neque explicabo,
+            libero quidem placeat, accusantium sit.</p>
+        <p>Voir plus sur le produit</p>
+        <div>
+            <h3>Version :</h3>
+            <p>50cl</p>
+            <p>1L</p>
+            <p>1.5L</p>
+        </div>
+        <h3>Choisir un type de produit</h3>
+        <div>
+            <img src="Image_bouteille.svg" alt="">
+            <img src="Image_bouteille.svg" alt="">
+            <img src="Image_bouteille.svg" alt="">
+            <img src="Image_bouteille.svg" alt="">
+        </div>
+    </article>
+    <article class="actionsProduit">
+        <h2>Vendu par Loïc Raison</h2>
+        <p>Plus d'article de Loïc Raison</p>
+        <hr>
+        <img src="localisation.svg" alt="">
+        <p>Livré a Clermont-ferrand 63000, 10 place saint-michel</p>
+        <img src="Conditions.svg" alt="">
+        <p>Consulter les conditions générales de vente</p>
+        <hr>
+        <div>
+            <div>
+                <h3>Quantité</h3>
+                <div>
+                    <img src="moins.svg" alt="">
+                    <h3>1</h3>
+                    <img src="plus.svg" alt="">
+                </div>  
+            </div>
+            <button>Ajouter au panier</button>
+            <button>Acheter maintenant</button>
+        </div>
+    </article>
+</section>
+</main>
+</html>
