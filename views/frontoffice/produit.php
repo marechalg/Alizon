18c1492894b3331cc342f739171fb2e08b3bd967
Zelirety <natuslanus@gmail.com>
2025-11-07 11:06:45 +0200

    Je veux juste aller chez evan (smash)

--- views/frontoffice/produit.php
+++ views/frontoffice/produit.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <!-- sass --watch views/styles/main.scss:public/style/css -->
+    <!-- sass --watch views/styles/main.scss:public/style.css -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Page du produit</title>
@@ -14,7 +14,7 @@
 <main>
 <section class="infoHautProduit">
     <article class="rectangleProduit">
-        <img src="flecheGauche.svg" alt="">
+        <img src="../../public/images/flecheGauche.svg" alt="">
         <div>
             <img id="imageBouteille" src="../../public/images/Image_bouteille.svg" alt="">
             <div id="lesCercles">
@@ -23,7 +23,7 @@
                 <div class="cercleNoir"></div>
             </div>
         </div>
-        <img src="flecheDroite.svg" alt="">
+        <img src="../../public/images/flecheDroite.svg" alt="">
     </article>
     <article class="infoPreviewProduit">
         <h1>Cidre coco d'issé</h1>
@@ -35,14 +35,14 @@
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus enim iure ratione voluptates
             eius doloremque obcaecati dignissimos ea porro exercitationem ex omnis reiciendis neque explicabo,
             libero quidem placeat, accusantium sit.</p>
-        <p>Voir plus sur le produit</p>
-        <div>
+        <p class="voirPlus">Voir plus sur le produit</p>
+        <div class="version">
             <h3>Version :</h3>
             <p>50cl</p>
             <p>1L</p>
             <p>1.5L</p>
         </div>
-        <h3>Choisir un type de produit</h3>
+        <h3>Choisir un type de produit :</h3>
         <div>
             <img src="../../public/images/Image_bouteille.svg" alt="">
             <img src="../../public/images/Image_bouteille.svg" alt="">
@@ -51,30 +51,39 @@
         </div>
     </article>
     <article class="actionsProduit">
-        <h2>Vendu par Loïc Raison</h2>
-        <p>Plus d'article de Loïc Raison</p>
-        <hr>
-        <img src="localisation.svg" alt="">
-        <p>Livré a Clermont-ferrand 63000, 10 place saint-michel</p>
-        <img src="Conditions.svg" alt="">
+    <h2>Vendu par Loïc Raison</h2>
+    <p>Plus d'article de Loïc Raison</p>
+    <hr>
+    <div class="ligneActions">
+        <img src="../../public/images/livraison-rapide3.svg" alt="">
+        <p>Livraison GRATUITE - Expédié par mondial relais. Arrivée entre le mar. 21 septembre - ven. 24 septembre</p>
+    </div>
+    <div class="ligneActions">
+        <img src="../../public/images/emplacement1.svg" alt="">
+        <p>Livré a Clermont-ferrand 63000, 10 place saint-michel</p>   
+    </div>
+    <div class="ligneActions">
+        <img src="../../public/images/termes-et-conditions1.svg" alt="">
         <p>Consulter les conditions générales de vente</p>
-        <hr>
+    </div>
+    <hr>
+    <div>
         <div>
+            <h3 class="bouton">Quantité</h3>
             <div>
-                <h3>Quantité</h3>
-                <div>
-                    <img src="moins.svg" alt="">
-                    <h3>1</h3>
-                    <img src="plus.svg" alt="">
-                </div>  
-            </div>
-            <button>Ajouter au panier</button>
-            <button>Acheter maintenant</button>
+                <img src="moins.svg" alt="">
+                <h3>1</h3>
+                <img src="plus.svg" alt="">
+            </div>  
         </div>
-    </article>
+        <button>Ajouter au panier</button>
+        <button>Acheter maintenant</button>
+    </div>
+</article>
 </section>
+
 <hr>
-<section>
+<section class="informations">
     <h2>Plus d'informations sur l'article</h2>
     <article>
         <h3>📌 Titre du produit</h3>
@@ -129,5 +138,6 @@
 </main>
 <footer>
     <?php include '../../views/frontoffice/partials/footerConnecte.php'; ?>
-</footer>
+</footer> 
+</body>
 </html>
