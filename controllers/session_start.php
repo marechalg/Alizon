f698f47f6e8606db189ba6ed6641950ed21e08ca
Nosaphi <evan.legarrec56@gmail.com>
2025-11-17 08:42:11 +0100

    test v10 bdd

--- controllers/session_start.php
+++ controllers/session_start.php
@@ -2,6 +2,6 @@
     session_start();  
     $id_session = session_id();
     $_SESSION['id_session'] = $id_session;
-    //header('Location: ../views/frontoffice/acceuilConnecte.php');
+    header('Location: ../views/frontoffice/acceuilConnecte.php');
     exit();
 ?>
