e288e7b041087a69d8c0a974a0b489422f3a3848
marechalg <guewenmarechal@protonmail.com>
2025-11-17 10:01:42 +0100

    fix: adjust footer inclusion order in accueil.php for proper rendering

--- views/backoffice/accueil.php
+++ views/backoffice/accueil.php
@@ -169,11 +169,11 @@ require_once '../../controllers/date.php';
                 </article>
                 <a href="./produits.php" title="Voir plus"><img src="/public/images/infoDark.svg"></a>
             </section>
+
+            <?php require_once './partials/footer.php' ?>
         </main>
 
         <?php require_once './partials/retourEnHaut.php' ?>
-
-        <?php require_once './partials/footer.php' ?>
 
         <script src="../../public/script.js"></script>
     </body>
