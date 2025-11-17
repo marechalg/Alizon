ec56535fc79362a6db102015d775be52e5cc4c23
pperche <perche.paolo@gmail.com>
2025-11-17 09:09:17 +0100

    UPDATE v3 incription.php

--- controllers/session_start.php
+++ controllers/session_start.php
@@ -1,4 +1,25 @@
 <?php
+
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+                
+        $pseudo = $_POST['pseudo'] ?? '';
+        $prenom = $_POST['prenom'] ?? '';
+        $nom = $_POST['nom'] ?? '';
+        $email = $_POST['email'] ?? '';
+        $num_tel = $_POST['telephone'] ?? '';
+        $mdp = $_POST['motdepasse'] ?? '';
+        $date_naissance = $_POST['birthdate'] ?? '';
+        
+        $nouveauClient = $pdo->query("INSERT INTO _client ('dateNaissance', 'prenom', 'nom', 'email', 'mdp', 'noTelephone','pseudo')
+        VALUES ('$date_naissance', '$prenom', '$nom', '$email', '$mdp', '$num_tel', '$pseudo')");
+
+        if ($pdo->query($nouveauClient) === false) {
+            throw new Exception("Erreur lors de la création d'un : " . implode(', ', $pdo->errorInfo()));
+        }
+
+                
+        }
+
     session_start();  
     $id_session = session_id();
     $_SESSION['id_session'] = $id_session;
